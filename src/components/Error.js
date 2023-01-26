9ee9bdcf53a5ba6af4c07f1052cf099d629530cf
Robin <guptarobin466@gmail.com>
2023-01-26 13:58:30 +0530

    Route error

---
+++ src/components/Error.js
@@ -0,0 +1,14 @@
+import {useRouteError} from 'react-router-dom';
+
+const Error = () => {
+    const {status, statusText} = useRouteError();
+    return (
+        <div>
+            <h1>Oops!!</h1>
+            <h2>status: {status} </h2>
+            <h2>Page {statusText}</h2>
+        </div>
+    );
+};
+
+export default Error;
