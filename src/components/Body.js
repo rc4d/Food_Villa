33ad319b8cb529f607ec449b5f85af7f40c612e0
Robin <guptarobin466@gmail.com>
2023-01-26 13:55:22 +0530

    main body of app

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,33 +1,55 @@
-import React, { useState } from "react";
-import { restaurantData } from '../constant';
+import React, { useEffect, useState } from "react";
 import RestaurantCard from "./RestaurantCard";
+import  Shimmer  from "../Shimmer";
+import { Link } from "react-router-dom";
+import { filterData } from "./utils/helper";
+import useOnline from "./utils/useOnline";
+import {RESTAURANT_LIST_API} from '../constant';
 
-function filterData(searchText, restaurants){
-    const filteredRestaurant = restaurants.filter(restaurant => {
-        return restaurant.data.name.toLowerCase().includes(searchText.toLowerCase());
-    });
-    console.log(filteredRestaurant);
-    return filteredRestaurant;
-}
 const Body = () => {
-    const [restaurant, setRestaurant] = useState(restaurantData);
+
+    const [allRestaurant, setAllRestaurant] = useState([]);
+    const [restaurant, setRestaurant] = useState([]);
     const [searchText, setSearchText] = useState("");
-    return (
+
+    useEffect(() =>{
+        getRestaurantList();
+    }, []);
+
+    const getRestaurantList = async () => {
+        const response = await fetch(RESTAURANT_LIST_API)
+                                .then((data) => data.json());
+            setAllRestaurant(response?.data?.cards[2]?.data?.data?.cards);
+            setRestaurant(response?.data?.cards[2]?.data?.data?.cards);
+
+    }
+
+    const online = useOnline();
+    if(!online){
+        return <h1>🔴 Offline, please check your internet connection!! </h1>;
+    }
+
+
+    
+    if(!allRestaurant || !restaurant){
+        return <Shimmer />;
+    }
+    return (restaurant.length === 0) ? <Shimmer /> : (
         <>
         <div className="search-container">
-            <h2>Search for your favourite Restaurant</h2>
-            <div class="InputContainer"><input type="text" className="search-input" placeholder="It just can't be pizza..." value={searchText} onChange={(e) => setSearchText(e.target.value)} /></div>
+            <h2>Search for your favourite Food or Restaurant</h2>
+            <div className="InputContainer"><input type="text" className="search-input" placeholder="It just can't be pizza..." value={searchText} onChange={(e) => setSearchText(e.target.value)} /></div>
             <button className="search-btn" 
                 onClick={
                     // console.log(filterData(searchText,restaurantData)),
-                    () => setRestaurant(filterData(searchText,restaurantData))}
+                    () => setRestaurant(filterData(searchText,allRestaurant))}
             >Search</button>
         </div>
         <div className="restaurant-cards">
             {
                 restaurant.map((res) => {
                     return (
-                        <RestaurantCard key={res.data.id} data={res.data} />
+                        <Link  key={res.data.id} to={"/restaurant/"+res.data.id}><RestaurantCard data={res.data} /></Link>
                     );
                 })
             }
