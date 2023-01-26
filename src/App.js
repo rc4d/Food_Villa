0ad4f8c034be890eb84a7ef700e69deab4b72dd1
Robin <guptarobin466@gmail.com>
2023-01-26 13:57:18 +0530

    Outlet and Routing

--- src/App.js
+++ src/App.js
@@ -2,16 +2,54 @@ import './index.css';
 import Body from "./components/Body";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Contact from './components/Contact';
+import About from './components/About';
+import Error from './components/Error';
+import RestaurantMenu from './components/RestaurantMenu';
+import Profile from './components/Profile';
 
+import { createBrowserRouter, Outlet } from 'react-router-dom';
 
 const App = () => {
     return (
         <>
         <Header />
-        <Body />
+        <Outlet />
         <Footer />
         </>
     )
-}
+};
+export const appRouter = createBrowserRouter([
+    {
+        path:"/",
+        element: <App />,
+        errorElement: <Error />,
+        children: [
+            {
+                path:"/",
+                element: <Body /> 
+            },
+            {
+                path:"/about",
+                element: <About />,
+                children:[{
+                    path:"profile",
+                    element: <Profile />
+                }]
+            },
+            {
+                path:"/contact",
+                element: <Contact />
+            },
+            {
+                path:"/restaurant/:resId",
+                element: <RestaurantMenu />
+            },
+        ],
+    }
+])
 
+// {<About />
+// <Body />
+// <Contact />}
 export default App;
