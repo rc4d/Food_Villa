0d47d332e813ae761b92565529ae38949f2fa60a
Robin <guptarobin466@gmail.com>
2023-01-26 23:57:14 +0530

    eslint fixed

--- src/components/utils/useRestraunt.js
+++ src/components/utils/useRestraunt.js
@@ -5,17 +5,18 @@ const useRestraunt = (resId) => {
     const [restaurantInfo, setRestaurantInfo] = useState(null);
 // Get data from api
 // eslint-disable-next-line
+const getRestaurantInfo = async () => {
+    const response  = await fetch(FETCH_MENU_URL + resId)
+    .then( data => data.json())
+    .catch(error => console.error(error));
+
+    setRestaurantInfo(response?.data);
+}
     useEffect(() => {
         getRestaurantInfo();
-    }, []);
+    }, [getRestaurantInfo]);
 
-    const getRestaurantInfo = async () => {
-        const response  = await fetch(FETCH_MENU_URL + resId)
-        .then( data => data.json())
-        .catch(error => console.error(error));
-
-        setRestaurantInfo(response?.data);
-    }
+    
 
     return restaurantInfo;
 };
